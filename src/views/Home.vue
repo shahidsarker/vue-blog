f9480f9465c9ae515f01d594170a66dcfb5c7a81
Shahid Sarker <shahid.sarker@gmail.com>
2022-01-25 12:47:01 -0500

    fetching data and reusable composable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,27 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts">toggle posts</button>
-    <button @click="posts.pop()">delete a post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading…</div>
   </div>
 </template>
 
 <script>
 import PostList from "../components/PostList.vue";
-import { ref } from "vue";
+import getPosts from "../composables/getPosts";
 
 export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: "Welcome to the blog",
-        body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam velit voluptatibus sed officiis dolor, harum culpa dolores incidunt autem exercitationem fuga tempore, odit quibusdam amet repellendus cumque? Iusto, maiores cumque.",
-        id: 1,
-      },
-      {
-        title: "Top 5 CSS Tips",
-        body: "Animi delectus ipsa, eius voluptatibus pariatur cupiditate amet soluta maxime doloribus, ab placeat, voluptas esse aut molestias beatae dicta debitis repellat at eligendi nemo velit. Excepturi quo unde veritatis alias!",
-        id: 2,
-      },
-    ]);
-    const showPosts = ref(true);
+    const { error, posts, load } = getPosts();
 
-    return { posts, showPosts };
+    load();
+
+    return { error, posts };
   },
 };
 </script>
